c763ebeb4b90a81a504d21274c9f4ed85fc8826b
Willem Mahy <willem.mahy@live.com>
2017-11-08 11:22:54 +0100

    Removed obsolete implementation from Home_MainMenu behavior

--- ArchiVR_KSArchitect/Assets/Scripts/WM/ArchiVR/Menu/Home_MainMenu.cs
+++ ArchiVR_KSArchitect/Assets/Scripts/WM/ArchiVR/Menu/Home_MainMenu.cs
@@ -6,29 +6,8 @@ using Assets.Scripts.WM.UI;
 
 public class Home_MainMenu : MonoBehaviour {
 
-    //! The toggle button to set the view mode.
-    public Button m_viewModeButton = null;
-
     // Use this for initialization
-    void Start() {
-        //InitViewModeButton();
-    }
-
-    void InitViewModeButton()
-    { 
-        var toggleButtonComponent = m_viewModeButton.GetComponent<ToggleButton>();
-
-        //toggleButtonComponent.LoadOptions(viewModeOptionSpritePaths);
-
-        m_viewModeButton.onClick.AddListener(ViewModeButton_OnClick);
-    }
-	
-	void ViewModeButton_OnClick()
+    void Start()
     {
-        var toggleButtonComponent = m_viewModeButton.GetComponent<ToggleButton>();
-
-        toggleButtonComponent.SetNextOption();
-
-
     }
 }
