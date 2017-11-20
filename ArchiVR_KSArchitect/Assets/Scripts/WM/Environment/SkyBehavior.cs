98816168ad2b3644f9fde22179136d39e6e4dd7c
Willem Mahy <willem.mahy@live.com>
2017-11-20 10:28:32 +0100

    Added FPS character controller

--- ArchiVR_KSArchitect/Assets/Scripts/WM/Environment/SkyBehavior.cs
+++ ArchiVR_KSArchitect/Assets/Scripts/WM/Environment/SkyBehavior.cs
@@ -16,7 +16,10 @@ namespace Assets.Scripts.WM.Environment
         // Update is called once per frame
         void Update()
         {
-            gameObject.transform.position = GameObject.Find("Main Camera").transform.position;
+            gameObject.transform.position = 
+                Camera.main
+                //GameObject.Find("Main Camera")
+                .transform.position;
         }
 
         void SetSkyBox(string materialPath) // eg: "SkyboxNoonCloudy01_2048/SkyboxNoonCloudy01_2048"
